287e18c6a4e979d4e5902cf61f89fb4441144e00
Thiago H.R. Costa <thiago_henrique86@hotmail.com>
2020-04-19 11:42:45 -0300

    Refatorando código

--- src/main/java/challenge/CriptografiaCesariana.java
+++ src/main/java/challenge/CriptografiaCesariana.java
@@ -5,71 +5,49 @@ import java.util.List;
 
 public class CriptografiaCesariana implements Criptografia {
 
-    public String mainCode(String texto){
+    public String mainCode(String texto) {
         String setLowerCase = texto.toLowerCase();
         return setLowerCase;
     }
 
-    public int txtSize(String texto){
+    public String checkTexto(String texto) {
+        if (texto.isEmpty()) throw new IllegalArgumentException("Error! Texto can´t be empty");
+        if (texto == null) throw new NullPointerException("Error! Texto can´t be null");
+
+        return texto;
+    }
+
+    private String doCryptoOrDecrypto(String texto, int encodeOrDecode) {
         int txtSize = texto.length();
-        return txtSize;
+        String newCode = "";
+
+        for (int i = 0; i < txtSize; i++) {
+
+            char letter = (char) (mainCode(texto).charAt(i) + encodeOrDecode);
+
+            if (Character.isWhitespace(mainCode(texto).charAt(i))) {
+                newCode += " ";
+            } else if (Character.isDigit(mainCode(texto).charAt(i))) {
+                newCode += mainCode(texto).charAt(i);
+            } else if (letter > 'z') {
+                newCode += (char) (mainCode(texto).charAt(i) - (26 - encodeOrDecode));
+            } else {
+                newCode += (char) (mainCode(texto).charAt(i) + encodeOrDecode);
+            }
+        }
+        return newCode;
     }
 
     @Override
     public String criptografar(String texto) {
-
-        if (texto.isEmpty()) throw new IllegalArgumentException("Error! Texto can´t be empty");
-        if (texto == null) throw new NullPointerException("Error! Texto can´t be null");
-
-        String newCode = "";
-        int encode = 3;
-
-        for (int i=0; i < txtSize(texto); i++){
-            char letter = (char) (mainCode(texto).charAt(i) + encode);
-
-            if (Character.isWhitespace(mainCode(texto).charAt(i))){
-                newCode += " ";
-            }
-            else if (Character.isDigit(mainCode(texto).charAt(i))){
-                newCode += mainCode(texto).charAt(i);
-            }
-            else if (letter > 'z'){
-                newCode += (char)(mainCode(texto).charAt(i) - (26 - encode));
-            }
-            else {
-                newCode += (char)(mainCode(texto).charAt(i) + encode);
-            }
-        }
-
-        return newCode;
+        checkTexto(texto);
+        return doCryptoOrDecrypto(texto, 3);
     }
 
     @Override
     public String descriptografar(String texto) {
-
-        if (texto.isEmpty()) throw new IllegalArgumentException("Error! Texto can´t be empty");
-        if (texto == null) throw new NullPointerException("Error! Texto can´t be null");
-
-        String newCode = "";
-        int decode = -3;
-
-        for (int i=0; i < txtSize(texto); i++){
-            char letter = (char) (mainCode(texto).charAt(i) + decode);
-
-            if (Character.isWhitespace(mainCode(texto).charAt(i))){
-                newCode += " ";
-            }
-            else if (Character.isDigit(mainCode(texto).charAt(i))){
-                newCode += mainCode(texto).charAt(i);
-            }
-            else if (letter > 'z'){
-                newCode += (char)(mainCode(texto).charAt(i) - (26 - decode));
-            }
-            else {
-                newCode += (char)(mainCode(texto).charAt(i) + decode);
-            }
-        }
-
-        return newCode;
+        checkTexto(texto);
+        return doCryptoOrDecrypto(texto, -3);
     }
 }
+
